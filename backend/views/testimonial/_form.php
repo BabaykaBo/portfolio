decfe85460dc81e8e18369523c54cde0a699886f
zaaqq135@gmail.com <Oleh>
2024-09-08 09:37:50 +0300

    adding star rating

--- backend/views/testimonial/_form.php
+++ backend/views/testimonial/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use kartik\form\ActiveForm;
 use kartik\widgets\FileInput;
+use kartik\widgets\StarRating;
 
 use yii\helpers\Url;
 
@@ -16,18 +17,18 @@ use yii\helpers\Url;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'project_id')->dropDownList($projects, ['prompt'=> '']) ?>
+    <?= $form->field($model, 'project_id')->dropDownList($projects, ['prompt' => '']) ?>
 
     <?= $form->field($model, 'imageFile')->widget(FileInput::class, [
-    'options' => ['accept' => 'image/*'],
-    'pluginOptions' => [
-        'initialPreview' => $model->imageAbsoluteUrl(),
-        'initialPreviewAsData' => true,
-        'showUpload' => false,
-        'deleteUrl' => Url::to('delete-customer-image'),
-        'initialPreviewConfig' => $model->imageConfig(),
-    ]
-]); ?>
+        'options' => ['accept' => 'image/*'],
+        'pluginOptions' => [
+            'initialPreview' => $model->imageAbsoluteUrl(),
+            'initialPreviewAsData' => true,
+            'showUpload' => false,
+            'deleteUrl' => Url::to('delete-customer-image'),
+            'initialPreviewConfig' => $model->imageConfig(),
+        ]
+    ]); ?>
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
@@ -35,7 +36,20 @@ use yii\helpers\Url;
 
     <?= $form->field($model, 'review')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'rating')->textInput() ?>
+    <?= StarRating::widget([
+        'model' => $model,
+        'attribute' => 'rating',
+        'pluginOptions' => [
+            'theme' => 'krajee-uni',
+            'filledStar' => '★',
+            'emptyStar' => '☆',
+            'step' => 1,
+            'min' => 0,
+            'minThreshold' => 1,
+            'max' => 5,
+
+        ]
+    ]); ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
