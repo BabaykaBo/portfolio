0488c493058bf9fcc041302b7b2427c4d51d1c28
zaaqq135@gmail.com <Oleh>
2024-09-15 17:48:40 +0300

    added env

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,13 +1,16 @@
 <?php
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->load();
+
 
 return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=basic_yii2',
-            'username' => 'oleh',
-            'password' => 'oleh',
-            'charset' => 'utf8',
+            'dsn' => $_ENV['MYSQL_DSN'],
+            'username' => $_ENV['MYSQL_USERNAME'],
+            'password' => $_ENV['MYSQL_PASSWORD'],
+            'charset' => $_ENV['MYSQL_CHARSET'],
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
